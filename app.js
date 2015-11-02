e6553b397653829b158b08f2acaa939b038cf71d
claire <cdersky@gmail.com>
2015-11-02 15:56:06 -0800

    MVP complete

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+var port = process.env.PORT || '3000';
+
+app.use(express.static(__dirname + '/'));
+
+http.listen(port, function() {
+  console.log('listening on *:' + port);
+});
